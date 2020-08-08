3398d4c01f4f3d424996950d5fa75cc5720494e0
Punkwalker <39736560+Punkwalker@users.noreply.github.com>
2020-08-07 19:47:17 -0500

    Import code for MySQL

--- MongoDB-Project2.py
+++ MongoDB-Project2.py
@@ -1,63 +1,118 @@
 import csv
 import json
-import mysql.connector
 
 from pymongo import MongoClient
 
-dbconnect = MongoClient('localhost', 27017)
+import mysql.connector
 
-def get_employee() :
-   db = dbconnect.company.employee
-   db.drop()
-   csvfile = open('Input/EMPLOYEE.csv', 'r')
-   col_name = [ "eFname", "eMI", "eLname", "eSsn", "eDOB", "eAddress", "eSex", "eSalary", "eSupSsn", "eDNo"]
-   for row in csvfile:
-      db.insert_many([{col_name[0]:row.split(',')[0],
-                 col_name[1]: row.split(',')[1],
-                 col_name[2]: row.split(',')[2],
-                 col_name[3]: row.split(',')[3],
-                 col_name[4]: row.split(',')[4],
-                 col_name[4]: row.split(',')[5],
-                 col_name[4]: row.split(',')[6],
-                 col_name[4]: row.split(',')[7],
-                 col_name[4]: row.split(',')[8],
-                 col_name[4]: row.split(',')[9],
-                 }])
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="punkwalker",
+  password="Anipropank@2210"
+)
 
-get_employee()
-dbconnect.close()
+dbcursor = mydb.cursor()
+dbcursor.execute("CREATE DATABASE IF NOT EXISTS company")
+
+# Create Department Table--------------------------------
+dbcursor.execute("DROP TABLE IF EXISTS company.department")
+
+createdept = "CREATE TABLE IF NOT EXISTS company.department (DName VARCHAR(45) NOT NULL," \
+             "DNum INT NOT NULL," \
+             "ManagerSSN INT NULL," \
+             "ManagerStartDt VARCHAR(45) NULL)"
+dbcursor.execute(createdept)
+
+csvfile = open('Input/DEPARTMENT.csv', 'r')
+insertdept = "INSERT INTO company.department (DName, DNum, ManagerSSN, ManagerStartDt) VALUES (%s, %s, %s,%s)"
+
+for row in csvfile:
+    val = tuple(row.rstrip('\n').replace("'", '').split(','))
+    dbcursor.execute(insertdept, val)
+
+mydb.commit()
+
+# Create Employee Table--------------------------------
+dbcursor.execute("DROP TABLE IF EXISTS company.employee")
+
+createemp = "CREATE TABLE IF NOT EXISTS company.employee (`EFname` VARCHAR(45) NOT NULL," \
+             " `EMiddle` VARCHAR(45) NULL," \
+             "`ELname` VARCHAR(45) NULL," \
+             "`ESSN` VARCHAR(45) NOT NULL," \
+             "`EDOB` VARCHAR(45) NOT NULL," \
+             "`EAddress` VARCHAR(45) NOT NULL," \
+             "`Sex` CHAR(1) NOT NULL," \
+             "`ESalary` INT NULL," \
+             "`SupSSN` VARCHAR(45) NULL," \
+             "`EDeptNo` INT NULL," \
+             "PRIMARY KEY (`ESSN`))"
+
+dbcursor.execute(createemp)
+
+csvfile = open('Input/EMPLOYEE.csv', 'r')
+insertemp = "INSERT INTO company.employee (EFname, EMiddle, ELname, ESSN, EDOB, EAddress, Sex, ESalary, SupSSN, EDeptNo) VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s, %s)"
+
+for row in csvfile:
+    EFname = row.rstrip('\n').replace("'", '').split(',')[0]
+    EMiddle = row.rstrip('\n').replace("'", '').split(',')[1]
+    ELname = row.rstrip('\n').replace("'", '').split(',')[2]
+    ESSN = row.rstrip('\n').replace("'", '').split(',')[3]
+    EDOB = row.rstrip('\n').replace("'", '').split(',')[4]
+    EAddress = row.rstrip('\n').replace("'", '').split(',')[5] +" "+ row.rstrip('\n').replace("'", '').split(',')[6] +" "+ row.rstrip('\n').replace("'", '').split(',')[7]
+    Sex = row.rstrip('\n').replace("'", '').split(',')[8]
+    ESalary = row.rstrip('\n').replace("'", '').split(',')[9]
+    SupSSN = row.rstrip('\n').replace("'", '').split(',')[10]
+    EDeptNo = row.rstrip('\n').replace("'", '').split(',')[11]
+    dbcursor.execute(insertemp, (EFname, EMiddle, ELname, ESSN, EDOB, EAddress, Sex, ESalary, SupSSN, EDeptNo))
+mydb.commit()
+
+# Create Department Table--------------------------------
+dbcursor.execute("DROP TABLE IF EXISTS company.department")
+
+createdept = "CREATE TABLE IF NOT EXISTS company.department (DName VARCHAR(45) NOT NULL," \
+             "DNum INT NOT NULL," \
+             "ManagerSSN INT NULL," \
+             "ManagerStartDt VARCHAR(45) NULL)"
+dbcursor.execute(createdept)
+
+csvfile = open('Input/DEPARTMENT.csv', 'r')
+insertdept = "INSERT INTO company.department (DName, DNum, ManagerSSN, ManagerStartDt) VALUES (%s, %s, %s,%s)"
+
+for row in csvfile:
+    val = tuple(row.rstrip('\n').replace("'", '').split(','))
+    dbcursor.execute(insertdept, val)
+
+mydb.commit()
 
 
-
-
-cnx = mysql.connector.connect(user='root', password='tiger',
-                              host='127.0.0.1',
-                              database='company')
-
-print("Connection created=")
-
-mycursor = cnx.cursor()
-
-mycursor.execute("SELECT * FROM department")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
-
-
-
-print("\n\n\n\Query 2==================================")
-myquery = "select PName,DName from project, department where project.PDeptNo = department.DNum"
-cursor1 = cnx.cursor()
-cursor1.execute(myquery)
-result = cursor1.fetchall()
-
-for x in result:
-  print(x)
-
-cnx.close()
-
-
-
-
+# dbconnect = MongoClient('localhost', 27017)
+#
+# def get_employee() :
+#    db = dbconnect.company.employee
+#    db.drop()
+#    csvfile = open('Input/EMPLOYEE.csv', 'r')
+#    col_name = [ "eFname", "eMI", "eLname", "eSsn", "eDOB", "eAddress", "eSex", "eSalary", "eSupSsn", "eDNo"]
+#    for row in csvfile:
+#       db.insert_many([{col_name[0]:row.split(',')[0],
+#                  col_name[1]: row.split(',')[1],
+#                  col_name[2]: row.split(',')[2],
+#                  col_name[3]: row.split(',')[3],
+#                  col_name[4]: row.split(',')[4],
+#                  col_name[5]: (row.split(',')[5]+row.split(',')[6]+row.split(',')[7]),
+#                  col_name[6]: row.split(',')[8],
+#                  col_name[7]: row.split(',')[9],
+#                  col_name[8]: row.split(',')[10],
+#                  col_name[9]: row.split(',')[11],
+#                  }])
+# # def print_emp() :
+# #     db = dbconnect.company.employee
+# #     employee_list = db.find({"eFname": "Joyce"})
+#
+#
+#
+# get_employee()
+# employee_list = dbconnect.company.employee.find({"eFname": "'Joyce'"})
+# for x in employee_list :
+#     print(x[2])
+#
+# dbconnect.close()
